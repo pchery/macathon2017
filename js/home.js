aaea79d57f05fe671be52fbbf10f55553725bd39
Hello again <deexpabada@gmail.com>
2017-11-11 05:28:56 -0600

    trending feature

--- js/home.js
+++ js/home.js
@@ -2,17 +2,17 @@ $(document).ready(function(){
 
     var strings = ["naut1", "naut2", "naut3","naut4", "naut5", "naut6", "naut7", "naut8"];
     var nauts = ["aut1", "aut2", "aut3","aut4", "aut5", "aut6", "aut7", "aut8"];
-    var active_posts = [ 
-        new Post("howdy 1", "askjdkasjdkasj", [1], ["hello1","hey1"], "LA", "thread1", 1, true),
-        new Post("howdy 2","asjdhaskjdask", [2], ["hello2","hey2"], "SF", "thread2", 2, true),
-        new Post("howdy 3", "aksdjkasjd", [3], ["hello3","hey3"], "MSP", "thread3", 3, true),
-        new Post("howdy 4", "asjdkasjdk", [4], ["hello4","hey4"], "PHX", "thread4", 4, true),
+    var active_posts = [
+        new Post("Wear Black!", "I'm getting sick of wearing boring colors everyday. I want to start wearing more red or pink.#colors #unique", [1], ["colors","unique"], "Los Angeles  - 73,231 - 11/11/17", "thread1", 1, true),
+        new Post("Nailpolish","I'm a guy and I want to be confident wearing nail polish.", [2], ["confidence","breaksocialnorms"], "Tokyo", "thread2", 2, true),
+        new Post("Random Chats", "Greet everyone who passes me on the street.", [3], ["mnnice","friendlyaf"], "St. Paul", "thread3", 3, true),
+        new Post("No phone subways", "I don't want to use my phone on the subway on my commute.", [4], ["mindfulness","offthegrid"], "Amsterdam", "thread4", 4, true),
     ]
 
-    var past_posts = [ 
-        new Post("yo 1", "aksjdaksd", [1], ["hello1","hey1"], "LA", "thread1", 1, false),
-        new Post("yo 2", "aksdjaksdj", [2], ["hello2","hey2"], "SF", "thread2", 2, false),
-        new Post("yo 3", "aksjdksad", [3], ["hello3","hey3"], "MSP", "thread3", 3, false),
+    var past_posts = [
+        new Post("Happier Thoughts", "I want to start thinking happier thoughts #goodvibes", [1], ["hello1","hey1"], "Seoul, South Korea - 08/23/17", "thread1", 1, false),
+        new Post("Wear yoga pants to work", "Casual Friday is everyday. I want to be comfortable at work. #comfy", [2], ["New York, New York - 09/23/17","hey2"], "New York, New York - 09/14/17", "thread2", 2, false),
+        new Post("Stand Up", "I want to speak up when the professor makes a racist remark, even by accident. #standup", [3], ["Colleges, Everywhere","hey3"], "Colleges, Everywhere - TODAY", "thread3", 3, false),
         new Post("yo 4", "askjdkasdj",[4], ["hello4","hey4"], "PHX", "thread4", 4, false),
     ]
     var j = 0;
@@ -42,8 +42,15 @@ $(document).ready(function(){
         }
         i += inc;
         if(i + 1 < active_posts.length){
-            $(".trending1").text(active_posts[i].titl);
-            $(".trending2").text(active_posts[i+1].titl);
+            //$(".trending1").text(active_posts[i].titl);
+            $(".trending1").find(".naut-title").text(active_posts[i].titl);
+            $(".trending1").find(".naut-location").text(active_posts[i].loc);
+            $(".trending1").find(".naut-body").text(active_posts[i].txt);
+            //$(".trending2").text(active_posts[i+1].titl);
+            $(".trending2").find(".naut-title").text(active_posts[i+1].titl);
+            $(".trending2").find(".naut-location").text(active_posts[i+1].loc);
+            $(".trending2").find(".naut-body").text(active_posts[i+1].txt);
+
         }
     })
     $(".b1").click(function(){
@@ -54,8 +61,13 @@ $(document).ready(function(){
         }
         i -= inc;
         if(i + 1 < active_posts.length && i >= 0){
-            $(".trending1").text(active_posts[i].titl);
-            $(".trending2").text(active_posts[i+1].titl);
+          $(".trending1").find(".naut-title").text(active_posts[i].titl);
+          $(".trending1").find(".naut-location").text(active_posts[i].loc);
+          $(".trending1").find(".naut-body").text(active_posts[i].txt);
+          //$(".trending2").text(active_posts[i+1].titl);
+          $(".trending2").find(".naut-title").text(active_posts[i+1].titl);
+          $(".trending2").find(".naut-location").text(active_posts[i+1].loc);
+          $(".trending2").find(".naut-body").text(active_posts[i+1].txt);
         }
     })
 
@@ -96,15 +108,15 @@ $(document).ready(function(){
     $(".past-nauts").mouseleave(function(){
         $(".past-nauts").css("opacity", "1");
     })
-    
-    $(".pledge").click(function(){
-        $(this).hide();
-        $(this).next().show();
-    })
-    $(".mission_pop").click(function(){
-        $(this).hide();
-        $(this).prev().show();
-    })
+
+    // $(".pledge").click(function(){
+    //     $(this).hide();
+    //     $(this).next().show();
+    // })
+    // $(".mission_pop").click(function(){
+    //     $(this).hide();
+    //     $(this).prev().show();
+    // })
 
 
 
@@ -114,7 +126,7 @@ function User(username, password, posted_ids, partic_ids){
     this.password = password;
     this.posted_ids = posted_ids;
     this.partic_ids = partic_ids;
-    this.posts = []; 
+    this.posts = [];
     this.loc = loc; //get location somehow
     this.post = function (text){
         posts.push(text, new Post([id], [], loc, new Thread(), id, true));
@@ -130,13 +142,13 @@ function User(username, password, posted_ids, partic_ids){
 }
 
 function Post(titl, text, participants, hashtags, loc, thread, creator, active) {
-    this.titl = titl; 
-    this.txt = text; 
+    this.titl = titl;
+    this.txt = text;
     this.participants = participants;
     this.hashtags = hashtags;
     this.loc = loc;
     this.thread = thread;
-    this.creator = creator; 
+    this.creator = creator;
     //this.id = id;
     this.flags;
     this.active;
@@ -144,7 +156,7 @@ function Post(titl, text, participants, hashtags, loc, thread, creator, active) 
     this.date = 1; //get
     this.add_partic = function(user_id){
         participants.push(user_id);
-        
+
     }
 }
 
@@ -157,4 +169,3 @@ function Comment(){
 }
 
 });
-
